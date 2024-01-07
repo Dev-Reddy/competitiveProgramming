874c92f004dbb8a2e484d390ac2b8c1f2210369c
DEV REDDY <devreddy4444@gmail.com>
2024-01-07 19:31:14 +0530

    Minor change in APLUSB

--- CodeChef/START115/APLUSB/sol.cpp
+++ CodeChef/START115/APLUSB/sol.cpp
@@ -3,10 +3,8 @@ using namespace std;
 
 void print(vector<int> v)
 {
-    for (int i = 0; i < v.size(); i++)
-    {
-        cout << v[i] << " ";
-    }
+    for (int x : v)
+        cout << x << " ";
     cout << endl;
 }
 
